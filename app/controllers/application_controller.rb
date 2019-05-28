ac07deabdbe2765786774c88d15837d2db175fd6
Michael Schiumo <mschiumo18@gmail.com>
2019-05-28 12:45:47 -0400

    coded out helper methods in app controller

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-
-
-  def current_user
-    if session[:rider_id].present?
-      rider = Rider.find_by(:id => session[:rider_id])
-    end
-  end    
-end
---
+++ app/controllers/concerns/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+  helper_method :current_user, :require_login, :admin_only
+
+  def current_user
+    if session[:rider_id].present?
+      rider = Rider.find_by(:id => session[:rider_id])
+    end
+  end
+
+  def require_login
+    unless current_user
+      redirect_to root_path
+    end
+  end
+
+  def admin_only
+    unless current_user.admin
+      flash[:notice] = "You must be an admin to access this page."
+      redirect_to rider_path(current_user)
+    end
+  end
+end
