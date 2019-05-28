a82c7d91f769dc460c7e8c4e51e20318481c6679
Michael Schiumo <mschiumo18@gmail.com>
2019-05-28 12:52:24 -0400

    added show and private methods to riders

--- app/controllers/concerns/riders_controller.rb
+++ app/controllers/concerns/riders_controller.rb
@@ -12,5 +12,16 @@ class RidersController < ApplicationController
     else
       render :new
     end
-  end        
+  end
+
+  def show
+    @rider = Rider.find_by(:id => params[:id])
+  end
+
+
+  private
+
+  def rider_params
+    params.require(:rider).permit(:name, :password, :experience, :stance, :ticket_number, :admin)
+  end           
 end
