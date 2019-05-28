7f0982bf5441bce78d61b4a113781a3931a21e99
Michael Schiumo <mschiumo18@gmail.com>
2019-05-28 12:47:52 -0400

    added create and new functions to riders controller

--- app/controllers/concerns/riders_controller.rb
+++ app/controllers/concerns/riders_controller.rb
@@ -1,3 +1,16 @@
 class RidersController < ApplicationController
 
+  def new
+    @rider = Rider.new
+  end
+
+  def create
+    rider = Rider.create(rider_params)
+    if rider.valid?
+      session[:id] = rider.id
+      redirect_to rider_path(rider)
+    else
+      render :new
+    end
+  end        
 end
