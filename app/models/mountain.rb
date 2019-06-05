f419f716da360fb1f3b89ee6b61d6d42f15fdc89
Michael Schiumo <mschiumo18@gmail.com>
2019-06-05 15:26:23 -0400

    added index view for trails

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -2,11 +2,5 @@ class Mountain < ApplicationRecord
   has_many :trails
   has_many :riders, through: :trails
 
-  # def self.top_ranked
-  #   order(:rank).last
-  # end
-
-  #or
-
   scope :top_ranked, -> { order(rank: :DESC).first }
 end
